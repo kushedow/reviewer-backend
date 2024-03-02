2d6ed8f5a027fa32dad15d2bfb011b7f86752d81
Gleb Kushedow <kushedow@gmail.com>
2024-03-03 00:46:22 +0400

    sending groups statistics to sheets

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 from json import JSONDecodeError
 import gspread
+from loguru import logger
 
 from fastapi import FastAPI, Query, Body, Request
 from gspread import SpreadsheetNotFound
 from openai import AsyncOpenAI
 from starlette.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-
 
 origins = [
     "https://student-care.sky.pro",
@@ -40,11 +40,32 @@ client = AsyncOpenAI(
     api_key=OPENAI_API_KEY,
 )
 
-# os.environ["REQUESTS_CA_BUNDLE"] = "cacert.pem"
+
+def __grab_keys_from_sheet(worksheet):
+    keys = worksheet.row_values(1)
+    keys.remove("ticket_id")
+    logger.debug(f"Groups found {keys}")
+    return keys
+
+
+def __count_avg_criteria_by_group(items, factor_name):
+
+    values = []
+
+    for item in items:
+        if item.get('group') == factor_name:
+            values.append(int(item.get("grade"))-3)
+
+    logger.debug(values)
+
+    if len(values) == 0:
+        return 0
+
+    return round(sum(values) / (len(values) * 2), 2)  #  percentage in factor completion (0-100)
+    # can change the formula to min in no-time
 
 @app.get("/checklist/{sheet_id}")
 async def get(sheet_id):
-
     try:
         # достаем файл
         file = gc.open_by_key(sheet_id)
@@ -71,7 +92,6 @@ async def get(sheet_id):
 
 @app.post("/generate")
 async def generate(request: Request):
-
     try:
         data = await request.json()
     except JSONDecodeError:
@@ -93,16 +113,44 @@ async def generate(request: Request):
 
 @app.post("/report/")
 async def save_report(request: Request):
-
     try:
         data = await request.json()
     except JSONDecodeError:
         return JSONResponse({"error": "incorrect JSON, try again"}, status_code=400)
 
-    if "checklist_data" not in data:
+    try:
+        checklist_data = data["checklist_data"].values()
+    except KeyError:
         return JSONResponse({"error": "incorrect query, key checklist_data expected"}, status_code=400)
 
-    if "sheet_id" not in data:
-        return JSONResponse({"error": "incorrect query, key checklist_url expected"}, status_code=400)
+    try:
+        sheet_id = data["sheet_id"]
+    except KeyError:
+        return JSONResponse({"error": "incorrect query, key sheet_id expected"}, status_code=400)
 
-    return {"response": {}}
+    try:
+
+        if "http" in sheet_id:
+            sheet_id = sheet_id.split("/")[5]
+            logger.debug(f"Link received, opening {sheet_id}")
+
+        file = gc.open_by_key(sheet_id)
+
+    except PermissionError:
+        return JSONResponse({"error": "file is not public"}, status_code=403)
+
+    except SpreadsheetNotFound:
+        return JSONResponse({"error": "file not found"}, status_code=404)
+
+    result_sheet = file.worksheet("results")
+
+    row_to_push = ["ticket_id"]
+
+    for result_key in __grab_keys_from_sheet(result_sheet):
+
+        average_percent = __count_avg_criteria_by_group(checklist_data, result_key)
+        row_to_push.append(average_percent)
+
+    result_sheet.append_row(row_to_push)
+
+    return {"response": row_to_push}
